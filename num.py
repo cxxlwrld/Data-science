4adf56af6a2362443e5146914380101d9765acfc
cxxlwrld <69635062+cxxlwrld@users.noreply.github.com>
2022-11-28 13:12:27 +0300

    Add files via upload

---
+++ num.py
@@ -0,0 +1,14 @@
+
+def regressionLine(x, y):
+    import numpy as np
+    x_squares = np.square(x)
+    xy_product = np.array(x) * np.array(y)
+    x_sum = np.sum(np.array(x))
+    y_sum = np.sum(np.array(y))
+    x_squares_sum = np.sum(x_squares)
+    xy_product_sum = np.sum(xy_product)
+    n = len(x)
+    b = (n * (xy_product_sum) - (x_sum * y_sum)) / ( n * (x_squares_sum) - np.square(x_sum))
+    a = (y_sum - b * x_sum ) / n
+    return  (np.around(b, 4), np.around(a, 4))
+
